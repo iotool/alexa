f16f85404399cbd04d7564d79c67583598e248b5
iotool <roha.mbox@gmail.com>
2017-12-07 00:56:06 +0100

    Add files via upload

---
+++ skill-sprueche-klopfer/model_de-DE.js
@@ -0,0 +1,37 @@
+{
+  "languageModel": {
+    "invocationName": "sprüche klopfer",
+    "intents": [
+      {
+        "name": "AMAZON.CancelIntent",
+        "samples": []
+      },
+      {
+        "name": "AMAZON.HelpIntent",
+        "samples": []
+      },
+      {
+        "name": "AMAZON.StopIntent",
+        "samples": []
+      },
+      {
+        "name": "SpruchIntent",
+        "samples": [
+          "spruch",
+          "ein spruch",
+          "einen spruch",
+          "sage mir spruch",
+          "sage mir einen spruch",
+          "sag mir ein spruch",
+          "gebe mir spruch",
+          "gebe mir einen spruch",
+          "gebe mir ein spruch",
+          "gib mir spruch",
+          "gib mir einen spruch",
+          "gib mir ein spruch"
+        ],
+        "slots": []
+      }
+    ]
+  }
+}
