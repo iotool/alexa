42a35ac1f9c0be524a4c8481ec32cb5004372a01
iotool <roha.mbox@gmail.com>
2017-12-21 00:58:05 +0100

    V9

--- skill-oracle-assistent/skill-oracle-assistent_lamda.js
+++ skill-oracle-assistent/skill-oracle-assistent_lamda.js
@@ -9,91 +9,149 @@ const APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
 const languageStrings = {
  'en': {
   translation: {
-   WELCOME_SPEECH: '<lang xml:lang="en-US">Welcome.</lang>',
-   WELCOME_CARD_HEAD: 'instructions - first use',
-   WELCOME_CARD_BODY: 'Alexa, ask "Oracle Assistent" about ORA [904] or ORA [12] thousand [154].',
-   SQLCODE_HELP_SPEECH: '<lang xml:lang="en-US">Say, Alexa ask Oracle Assistent about Ora 904, or Ora 12 thousand 154.</lang>',
-   SQLCODE_ERROR_SPEECH: '<lang xml:lang="en-US">Ohh. I did not understand you!</lang>',
-   SQLCODE_NOTFOUND_SPEECH: '<lang xml:lang="en-US">Ohh. I did not know this SQL-code!</lang>',
-   HELP_MESSAGE: 'You can say tell me a space fact, or, you can say exit... What can I help you with?',
-   HELP_REPROMPT: 'What can I help you with?',
-   STOP_MESSAGE: 'Goodbye!',
+   LAUCH_VOICE: '<lang xml:lang="en-US">You are welcome to the Oracle assistant. How can I help you? Do you have an error message or a syntax question?</lang>',
+   SKILLINFO_VOICE: '<lang xml:lang="en-US">Release nine of this skill. The Oracle Assistant demonstrates the possibilities of voice-driven guidebooks. More information and feedback on this skill can be found in the Alexa app.</lang>',
+   SKILLINFO_CARDHEAD: 'Skill Info (Release 9)',
+   SKILLINFO_CARDBODY: 'The Oracle Assistant demonstrates the possibilities of voice-driven guidebooks. Your feedback about this skill in the Alexa app would be very nice ;-)',
+   STARTERROR_VOICE: '<lang xml:lang="en-US">Say, <prosody rate="x-slow"><say-as interpret-as="characters">ORA</say-as></prosody> 609, to get more about SQL-Code Ora 609. What error do you have?</lang>',
+   STARTSYNTAX_VOICE: '<lang xml:lang="en-US">Say, syntax of "order by", to get syntactical rules of sort order. Which syntax are you looking for?</lang>',
+   ORAERROR_NULL_VOICE: '<lang xml:lang="en-US">Ohh. I did not understand you. Say, <prosody rate="x-slow"><say-as interpret-as="characters">ORA</say-as></prosody> 609, to get more about SQL-Code Ora 609. What error do you have?</lang>',
+   ORAERROR_UNKNOWN_VOICE: '<lang xml:lang="en-US">I do not know this code. Which error should I seek?</lang>',
+   HELP_VOICE: '<lang xml:lang="en-US">Say error message or say syntax question. What do you want?</lang>',
+   CANCEL_VOICE: '<lang xml:lang="en-US">Goodbye, see you next time.</lang>',
+   STOP_VOICE: '<lang xml:lang="en-US">bye Bye. <break time="1s"/>Your feedback on the Oracle Assistant is possible via the Alexa app.</lang>',
+   UNHANDLED_VOICE: '<lang xml:lang="en-US">Oops, I did not understand you. Can you repeat please or say help!</lang>',
+   OVERANDOUT_VOICE: '<lang xml:lang="en-US">over and out.</lang>', 
+   NEXTQUESTION_VOICE: '<lang xml:lang="en-US">Which error or syntax question do you have yet?</lang>'
   },
  },
  'de': {
  translation: {
-   WELCOME_SPEECH: '<lang xml:lang="de-DE">Willkommen.</lang>',
-   WELCOME_CARD_HEAD: 'Anleitung - erste Nutzung',
-   WELCOME_CARD_BODY: 'Alexa, frage "Oracle Assistent" nach ORA [904] oder ORA [12] tausend [154].',
-   SQLCODE_HELP_SPEECH: '<lang xml:lang="de-DE">Sage, Alexa frage Oracle Assistent nach Ora neun null vier, oder Ora 12 tausend 154.</lang>',
-   SQLCODE_ERROR_SPEECH: '<lang xml:lang="de-DE">Ohh. Ich habe dich nicht verstanden!</lang>',
-   SQLCODE_NOTFOUND_SPEECH: '<lang xml:lang="de-DE">Ohh. Ich kenne diesen SQL-Code nicht!</lang>',
-   HELP_MESSAGE: 'Sage: Alexa, frage Oracle Assistent nach ORA 904',
-   HELP_REPROMPT: 'Wie kann ich dir helfen?',
-   STOP_MESSAGE: 'Auf Wiedersehen!',
+   LAUCH_VOICE: '<lang xml:lang="de-DE">Hallo, ich bin der Orakel Assistent und helfe dir bei Fragen zur Datenbank. Was hast du? Eine Fehlermeldung oder eine Syntaxfrage?</lang>',
+   SKILLINFO_VOICE: '<lang xml:lang="de-DE">Skill Version neun. Der Oracle Assistent demonstriert die Möglichkeiten von sprachgesteuerten Ratgebern. Mehr Informationen und Feedback zu diesem Skill findest du in der Alexa App.</lang>',
+   SKILLINFO_CARDHEAD: 'Skill Info (Version 9)',
+   SKILLINFO_CARDBODY: 'Der Oracle Assistent demonstriert die Möglichkeiten von sprachgesteuerten Ratgebern. Dein Feedback zum Skill in der Alexa App würde mich sehr freuen ;-)',
+   STARTERROR_VOICE: '<lang xml:lang="de-DE">Sage Ora 609, um zum Beispiel den SQL-Kot Ora 609 zu suchen. Welchen Fehler hast du?</lang>',
+   STARTSYNTAX_VOICE: '<lang xml:lang="de-DE">Sage Syntax Sortierung, um zum Beispiel die Ergebnisse von Abfragen zu sortieren. Welche Syntax suchst du?</lang>',
+   ORAERROR_NULL_VOICE: '<lang xml:lang="de-DE">Ohh. Ich habe dich nicht richtig verstanden. Sage Ora 609, um zum Beispiel mehr über SQL-Kot Ora 609 abzufragen. Welchen Fehler hast du?</lang>',
+   ORAERROR_UNKNOWN_VOICE: '<lang xml:lang="de-DE">Ich kenne diesen Fehlercode nicht! Welchen Fehler kann ich suchen?</lang>',
+   HELP_VOICE: '<lang xml:lang="de-DE">Sage Fehlermeldung oder sage Syntaxfrage. Was möchtest du?</lang>',
+   CANCEL_VOICE: '<lang xml:lang="de-DE">Auf Wiedersehen, bis zum nächsten Mal.</lang>',
+   STOP_VOICE: '<lang xml:lang="de-DE">Tschau. <break time="1s"/>Dein Feedback zum Oracle Assistenten ist über die Alexa App möglich.</lang>',
+   UNHANDLED_VOICE: '<lang xml:lang="de-DE">Ähh, ich habe dich leider nicht verstanden. Kannst du das bitte wiederholen oder sage Hilfe!</lang>',
+   OVERANDOUT_VOICE: '<lang xml:lang="de-DE">Ende der Durchsage.</lang>', 
+   NEXTQUESTION_VOICE: '<lang xml:lang="de-DE">Welche Fehlermeldung oder Syntaxfrage hast du noch?</lang>'
   },
  },
 };
 
 const handlers = {
   'LaunchRequest': function () {
-    const speechOutput = this.t('WELCOME_SPEECH')+this.t('SQLCODE_HELP_SPEECH');
-    const cardHead = this.t('WELCOME_CARD_HEAD');
-    const cardBody = this.t('WELCOME_CARD_BODY');
-    this.emit(':tellWithCard', speechOutput, cardHead, cardBody);
+    this.attributes['dialogMode'] = 'Y'; 
+    this.emit(':ask', this.t('LAUCH_VOICE'));
   },
-  'OraErrorIntent': function () {
+  'AllgemeineInformation_Intent': function () {
+    this.emit(':tellWithCard', this.t('SKILLINFO_VOICE'), this.t('SKILLINFO_CARDHEAD'), this.t('SKILLINFO_CARDBODY'));
+  },
+  'EinstiegFehlermeldung_Intent': function () {
+    this.emit(':ask', this.t('STARTERROR_VOICE'));
+  },
+  'EinstiegSyntaxfrage_Intent': function () {
+    this.emit(':ask', this.t('STARTSYNTAX_VOICE'));
+  },
+  'OraErrorA_Intent': function () {
     if (!this.event.request.intent.slots.sqlcode.value) {
-      const speechOutput = this.t('SQLCODE_ERROR_SPEECH')+this.t('SQLCODE_HELP_SPEECH');
-      this.emit(':tell', speechOutput);
+      const speechOutput = 
+      this.emit(':ask', this.t('ORAERROR_NULL_VOICE'));
+    } else if (this.event.request.intent.slots.sqlcode.value == '?') {
+      this.emit(':ask', this.t('ORAERROR_NULL_VOICE'));
+      this.emit(':ask', speechOutput);
     } else {     
       var sqlcode = this.event.request.intent.slots.sqlcode.value;
       var oraerror = getOraErrorByCode('ORA',sqlcode);
       if (!oraerror.ErrorType) {
-        this.emit(':tell', this.t('SQLCODE_NOTFOUND_SPEECH'));
+        this.emit(':ask', 'Ora '+sqlcode+'. '+this.t('ORAERROR_UNKNOWN_VOICE'));
       } else {
-        const speechOutput = 'ORA '+sqlcode+' '
-          +'<lang xml:lang="en-US">'+oraerror.English.Name+'</lang> '
-          +'<lang xml:lang="en-US">'+oraerror.English.Cause+'</lang> '
-          +'<lang xml:lang="en-US">'+oraerror.English.Action+'</lang> '
-        ;
-        this.emit(':tell', speechOutput);
+        var speechOutput = 'ORA '+sqlcode;
+        if (oraerror.English) {if (oraerror.English.Name) speechOutput += ' <lang xml:lang="en-US">'+oraerror.English.Name+'</lang>';}
+        if (oraerror.German && (this.event.request.locale.indexOf("de")>=0)) {if (oraerror.German.Cause) speechOutput += ' <break time="1s"/>'+oraerror.German.Cause;}
+        else if (oraerror.English) {if (oraerror.English.Cause) speechOutput += ' <break time="1s"/>'+oraerror.English.Cause;}
+        if (oraerror.German && (this.event.request.locale.indexOf("de")>=0)) {if (oraerror.German.Action) speechOutput += ' <break time="1s"/>'+oraerror.German.Action;}
+        else if (oraerror.English) {if (oraerror.English.Action) speechOutput += ' <break time="1s"/>'+oraerror.English.Action;}
+        if (this.attributes['dialogMode'] && (this.attributes['dialogMode'] == 'Y')) {
+          speechOutput += ' <break time="1s"/>'+this.t('NEXTQUESTION_VOICE');
+          this.emit(':ask', speechOutput);
+        } else {
+          speechOutput += ' <break time="1s"/>'+this.t('OVERANDOUT_VOICE');
+          this.emit(':tell', speechOutput);
+        }
       }
     }
   },
-  'OraTwoErrorIntent': function () {
+  'OraErrorB_Intent': function () {
     if ((!this.event.request.intent.slots.sqlcode_a.value) 
-      ||(!this.event.request.intent.slots.sqlcode_b.value)) {
-      const speechOutput = this.t('SQLCODE_ERROR_SPEECH')+this.t('SQLCODE_HELP_SPEECH');
-      this.emit(':tell', speechOutput);
+      &&(!this.event.request.intent.slots.sqlcode_b.value)) {
+      this.emit(':ask', this.t('ORAERROR_NULL_VOICE'));
+      this.emit(':ask', speechOutput);
+    } else if (this.event.request.intent.slots.sqlcode_a.value
+       && this.event.request.intent.slots.sqlcode_b.value
+       && this.event.request.intent.slots.sqlcode_a.value == '?'
+       && this.event.request.intent.slots.sqlcode_b.value == '?') {
+      this.emit(':ask', this.t('ORAERROR_NULL_VOICE'));
+      this.emit(':ask', speechOutput);
     } else {
-      var sqlcode_a = this.event.request.intent.slots.sqlcode_a.value;
-      var sqlcode_b = this.event.request.intent.slots.sqlcode_b.value;
+      var sqlcode_a = 0;
+      var sqlcode_b = 0;
+      if (!this.event.request.intent.slots.sqlcode_a.value) {
+        sqlcode_a = 1;
+      } else if (this.event.request.intent.slots.sqlcode_a.value == '?') {
+        sqlcode_a = 0;
+      } else {
+        sqlcode_a = this.event.request.intent.slots.sqlcode_a.value;
+      }
+      if (!this.event.request.intent.slots.sqlcode_b.value) {
+        sqlcode_b = 0;
+      } else if (this.event.request.intent.slots.sqlcode_b.value == '?') {
+        sqlcode_b = 0;
+      } else {
+        sqlcode_b = this.event.request.intent.slots.sqlcode_b.value;
+      }
       var sqlcode = parseInt(sqlcode_a)*1000+parseInt(sqlcode_b);
       var oraerror = getOraErrorByCode('ORA',sqlcode);
       if (!oraerror.ErrorType) {
-        this.emit(':tell', this.t('SQLCODE_NOTFOUND_SPEECH'));
+        this.emit(':ask', 'Ora '+sqlcode+'. '+this.t('ORAERROR_UNKNOWN_VOICE'));
       } else {
-        const speechOutput = 'ORA '+sqlcode+' '
-          +'<lang xml:lang="en-US">'+oraerror.English.Name+'</lang> '
-          +'<lang xml:lang="en-US">'+oraerror.English.Cause+'</lang> '
-          +'<lang xml:lang="en-US">'+oraerror.English.Action+'</lang> '
-        ;
-        this.emit(':tell', speechOutput);
+        var speechOutput = 'ORA '+sqlcode;
+        if (oraerror.English) {if (oraerror.English.Name) speechOutput += ' <lang xml:lang="en-US">'+oraerror.English.Name+'</lang>';}
+        if (oraerror.German && (this.event.request.locale.indexOf("de")>=0)) {if (oraerror.German.Cause) speechOutput += ' <break time="1s"/>'+oraerror.German.Cause;}
+        else if (oraerror.English) {if (oraerror.English.Cause) speechOutput += ' <break time="1s"/>'+oraerror.English.Cause;}
+        if (oraerror.German && (this.event.request.locale.indexOf("de")>=0)) {if (oraerror.German.Action) speechOutput += ' <break time="1s"/>'+oraerror.German.Action;}
+        else if (oraerror.English) {if (oraerror.English.Action) speechOutput += ' <break time="1s"/>'+oraerror.English.Action;}
+        if (this.attributes['dialogMode'] && (this.attributes['dialogMode'] == 'Y')) {
+          speechOutput += ' <break time="1s"/>'+this.t('NEXTQUESTION_VOICE');
+          this.emit(':ask', speechOutput);
+        } else {
+          speechOutput += ' <break time="1s"/>'+this.t('OVERANDOUT_VOICE');
+          this.emit(':tell', speechOutput);
+        }
       }
     }
   },
   'AMAZON.HelpIntent': function () {
-   const speechOutput = this.t('HELP_MESSAGE');
-   const reprompt = this.t('HELP_REPROMPT');
-   this.emit(':ask', speechOutput, reprompt);
+    this.emit(':ask', this.t('HELP_VOICE'));
   },
   'AMAZON.CancelIntent': function () {
-   this.emit(':tell', this.t('STOP_MESSAGE'));
+    this.emit(':tell', this.t('CANCEL_VOICE'));
   },
   'AMAZON.StopIntent': function () {
-   this.emit(':tell', this.t('STOP_MESSAGE'));
+    this.emit(':tell', this.t('STOP_VOICE'));
   },
+  'SessionEndedRequest': function () {
+    console.log('session ended!');
+  },
+  'Unhandled': function() {
+    this.emit(':ask', whit.t('UNHANDLED_VOICE'));
+  }
 };
 
 exports.handler = function (event, context) {
@@ -153,13 +211,13 @@ function getOraErrorByCode(pErrorType,pErrorNumber) {
   lIndexData = null;
   if (lFileEnglish) {
     lResponse.English = getOraErrorDetailByCode(lFileEnglish,pErrorType,pErrorNumber);
-    if (lResponse.English.Type) lResponse.ErrorType = lResponse.English.Type;
-    if (lResponse.English.Number) lResponse.ErrorNumber = lResponse.English.Number;
+    if (!lResponse.ErrorType && lResponse.English.Type) lResponse.ErrorType = lResponse.English.Type;
+    if (!lResponse.ErrorNumber && lResponse.English.Number) lResponse.ErrorNumber = lResponse.English.Number;
   }
   if (lFileGerman) {
     lResponse.German = getOraErrorDetailByCode(lFileGerman,pErrorType,pErrorNumber);
-    if (lResponse.German.Type) lResponse.ErrorType = lResponse.German.Type;
-    if (lResponse.German.Number) lResponse.ErrorNumber = lResponse.German.Number;
+    if (!lResponse.ErrorType && lResponse.German.Type) lResponse.ErrorType = lResponse.German.Type;
+    if (!lResponse.ErrorNumber && lResponse.German.Number) lResponse.ErrorNumber = lResponse.German.Number;
   }
   return lResponse;
 }
--- skill-oracle-assistent/skill-oracle-assistent_model-de.js
+++ skill-oracle-assistent/skill-oracle-assistent_model-de.js
@@ -1,5 +1,6 @@
 {
   "languageModel": {
+    "invocationName": "oracle assistent",
     "intents": [
       {
         "name": "AMAZON.CancelIntent",
@@ -14,57 +15,136 @@
         "samples": []
       },
       {
-        "name": "OraErrorIntent",
+        "name": "AllgemeineInformation_Intent",
         "samples": [
-          "ora {sqlcode}",
-          "code {sqlcode}",
-          "error {sqlcode}",
-          "fehler {sqlcode}",
-          "SQL code {sqlcode}"
+          "impressum",
+          "informationen zum skill",
+          "version vom skill",
+          "version des skills",
+          "skillversion",
+          "skill version",
+          "skillrelease",
+          "skill relase",
+          "imprint",
+          "information about skill",
+          "information about the skill",
+          "version of skill",
+          "version of the skill"
         ],
         "slots": [
-          {
-            "name": "sqlcode",
-            "type": "AMAZON.NUMBER"
-          }
         ]
       },
       {
-        "name": "OraTwoErrorIntent",
+        "name": "EinstiegFehlermeldung_Intent",
         "samples": [
-          "ora {sqlcode_a} tausend {sqlcode_b}",
-          "code {sqlcode_a} tausend {sqlcode_b}",
-          "error {sqlcode_a} tausend {sqlcode_b}",
-          "fehler {sqlcode_a} tausend {sqlcode_b}",
-          "SQL code {sqlcode_a} tausend {sqlcode_b}",
-          "ora {sqlcode_a} tausend und {sqlcode_b}",
-          "code {sqlcode_a} tausend und {sqlcode_b}",
-          "error {sqlcode_a} tausend und {sqlcode_b}",
-          "fehler {sqlcode_a} tausend und {sqlcode_b}",
-          "SQL code {sqlcode_a} tausend und {sqlcode_b}",
-          "ora {sqlcode_a} thousand {sqlcode_b}",
-          "code {sqlcode_a} thousand {sqlcode_b}",
-          "error {sqlcode_a} thousand {sqlcode_b}",
-          "fehler {sqlcode_a} thousand {sqlcode_b}",
-          "SQL code {sqlcode_a} thousand {sqlcode_b}",
-          "ora {sqlcode_a} thousand and {sqlcode_b}",
-          "code {sqlcode_a} thousand and {sqlcode_b}",
-          "error {sqlcode_a} thousand and {sqlcode_b}",
-          "fehler {sqlcode_a} thousand and {sqlcode_b}",
-          "SQL code {sqlcode_a} thousand and {sqlcode_b}"
+          "fehlermeldung",
+          "eine fehlermeldung",
+          "ich habe eine fehlermeldung",
+          "habe eine fehlermeldung",
+          "ich habe ein problem mit einem fehler",
+          "ich habe ein problem mit einer fehlermeldung",
+          "errormessage",
+          "an error message",
+          "a error message",
+          "i had an error",
+          "i had a error",
+          "i had error",
+          "i has an error",
+          "i has a error",
+          "i has error",
+          "i have an error",
+          "i have a error",
+          "i have error"
         ],
         "slots": [
-          {
-            "name": "sqlcode_a",
-            "type": "AMAZON.NUMBER"
-          },
-          {
-            "name": "sqlcode_b",
-            "type": "AMAZON.NUMBER"
-          }
+        ]
+      },
+      {
+        "name": "EinstiegSyntaxfrage_Intent",
+        "samples": [
+          "syntaxfrage",
+          "eine syntaxfrage",
+          "ich habe eine syntaxfrage",
+          "habe eine syntaxfrage",
+          "syntaxproblem",
+          "ein syntaxproblem",
+          "ich habe ein syntaxproblem",
+          "habe ein syntaxproblem",
+          "ich habe eine frage zur syntax",
+          "ich habe ein problem mit der syntax",
+          "ich ein problem mit der syntax",
+          "ich kenne die syntax nicht"
+        ],
+        "slots": [
+        ]
+      },
+      {
+        "name": "OraErrorA_Intent",
+        "samples": [
+          "ora",
+          "sql code",
+          "s q l code",
+          "sql kot",
+          "s q l kot",
+          "ora {sqlcode}",
+          "error {sqlcode}",
+          "fehler {sqlcode}",
+          "sql code {sqlcode}",
+          "s q l code {sqlcode}",
+          "sql kot {sqlcode}",
+          "s q l kot {sqlcode}"
+        ],
+        "slots": [
+          { "name":"sqlcode", "type":"AMAZON.NUMBER" }
+        ]
+      },
+      {
+        "name": "OraErrorB_Intent",
+        "samples": [
+          "ora {sqlcode_a} tausend {sqlcode_b}",
+          "error {sqlcode_a} tausend {sqlcode_b}",
+          "fehler {sqlcode_a} tausend {sqlcode_b}",
+          "sql code {sqlcode_a} tausend {sqlcode_b}",
+          "s q l code {sqlcode_a} tausend {sqlcode_b}",
+          "sql kot {sqlcode_a} tausend {sqlcode_b}",
+          "s q l kot {sqlcode_a} tausend {sqlcode_b}",
+          "ora {sqlcode_a} tausend und {sqlcode_b}",
+          "error {sqlcode_a} tausend und {sqlcode_b}",
+          "fehler {sqlcode_a} tausend und {sqlcode_b}",
+          "sql code {sqlcode_a} tausend und {sqlcode_b}",
+          "s q l code {sqlcode_a} tausend und {sqlcode_b}",
+          "sql kot {sqlcode_a} tausend und {sqlcode_b}",
+          "s q l kot {sqlcode_a} tausend und {sqlcode_b}",
+          "ora {sqlcode_a} thousand {sqlcode_b}",
+          "error {sqlcode_a} thousand {sqlcode_b}",
+          "fehler {sqlcode_a} thousand {sqlcode_b}",
+          "sql code {sqlcode_a} thousand {sqlcode_b}",
+          "s q l code {sqlcode_a} thousand {sqlcode_b}",
+          "ora {sqlcode_a} thousand and {sqlcode_b}",
+          "error {sqlcode_a} thousand and {sqlcode_b}",
+          "fehler {sqlcode_a} thousand and {sqlcode_b}",
+          "sql code {sqlcode_a} thousand and {sqlcode_b}",
+          "s q l code {sqlcode_a} thousand and {sqlcode_b}",
+          "ora ein tausend {sqlcode_b}",
+          "error ein tausend {sqlcode_b}",
+          "fehler ein tausend {sqlcode_b}",
+          "sql code ein tausend {sqlcode_b}",
+          "s q l code ein tausend {sqlcode_b}",
+          "sql kot ein tausend {sqlcode_b}",
+          "s q l kot ein tausend {sqlcode_b}",
+          "ora ein tausend und {sqlcode_b}",
+          "error ein tausend und {sqlcode_b}",
+          "fehler ein tausend und {sqlcode_b}",
+          "sql code ein tausend und {sqlcode_b}",
+          "s q l code ein tausend und {sqlcode_b}",
+          "sql kot ein tausend und {sqlcode_b}",
+          "s q l kot ein tausend und {sqlcode_b}"
+        ],
+        "slots": [
+          { "name":"sqlcode_a", "type":"AMAZON.NUMBER" },
+          { "name":"sqlcode_b", "type":"AMAZON.NUMBER" }
         ]
       }
-    ],
-    "invocationName": "oracle assistent"
+    ]
   }
 }
